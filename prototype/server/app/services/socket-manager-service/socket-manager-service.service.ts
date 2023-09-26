342e31563970ecb7553b2e2c8db66d58c90563e5
SamyLabassi <samy.labassi@gmail.com>
2023-09-26 10:45:11 -0400

    fixed disconnect message

--- prototype/server/app/services/socket-manager-service/socket-manager-service.service.ts
+++ prototype/server/app/services/socket-manager-service/socket-manager-service.service.ts
@@ -1,7 +1,6 @@
 import { LoggerService } from '@app/services/logger-service/logger.service';
 import { UserManagerService } from '@app/services/prototype-services/user-manager-service.service';
 import { Message } from '@common/prototype/message';
-import { User } from '@common/prototype/user';
 import { SocketEvent } from '@common/socket-event';
 import * as http from 'http';
 import { Server, Socket } from 'socket.io';
@@ -9,13 +8,10 @@ import { Service } from 'typedi';
 @Service()
 export class SocketManagerService {
     private sio: Server;
-    private rooms: Map<string, User[]>;
     private messages: Message[];
 
     // eslint-disable-next-line max-params -- all services are needed
     constructor(private userManagerService: UserManagerService, private logger: LoggerService) {
-        this.rooms = new Map<string, User[]>();
-        this.rooms.set('allChatProto', []);
         this.messages = [];
     }
 
@@ -30,24 +26,28 @@ export class SocketManagerService {
         this.sio.on(SocketEvent.Connection, (socket: Socket) => {
             this.logger.logInfo(`new user connected with : ${socket.id}`);
 
-            socket.on(SocketEvent.Disconnect, () => {
+            socket.on('disconnect', () => {
                 this.logger.logWarning(`user disconnected with id : ${socket.id}`);
                 this.userManagerService.removeUser(socket.id);
             });
 
             socket.on(SocketEvent.DisconnectFromChat, () => {
                 this.userManagerService.removeUser(socket.id);
+                this.logger.logWarning(`user left chat with id : ${socket.id}`);
             });
 
             socket.on(SocketEvent.Authenticate, (userName: string) => {
                 try {
                     this.userManagerService.addUser(socket.id, userName);
+
                     socket.join('allChatProto');
-                    this.rooms.get('allChatProto')?.push({ username: userName });
+
                     socket.emit(SocketEvent.UserAuthenticated);
-                    this.logger.logInfo(`authenticate successful, ${userName}`);
+
+                    this.logger.logInfo(`authenticate successful`);
                 } catch (e) {
                     socket.emit(SocketEvent.UserExists);
+
                     this.logger.logError(e);
                 }
             });
