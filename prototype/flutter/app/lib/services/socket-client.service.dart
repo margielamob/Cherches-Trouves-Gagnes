acaced1e84a868164487c1b849c90b5dea757627
Skander Hannachi <skander@Sansfil-Eduroam-Etudiants-0-111.polymtl.ca>
2023-09-28 12:40:35 -0400

    warn: I changed the server url for my macs home IP

--- prototype/flutter/app/lib/services/socket-client.service.dart
+++ prototype/flutter/app/lib/services/socket-client.service.dart
@@ -2,7 +2,7 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketClient {
   late IO.Socket socket;
-  String serverUrl = 'http://localhost:3000';
+  String serverUrl = 'http://10.0.0.9:3000';
 
   SocketClient() {
     socket = IO.io(serverUrl, <String, dynamic>{
