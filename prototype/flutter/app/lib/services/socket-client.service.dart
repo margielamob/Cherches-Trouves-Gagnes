8dd65b2379574a2bfd6651aa672890c75c463af8
Skander Hannachi <skander@Skanders-MacBook-Air.local>
2023-09-29 01:08:09 -0400

    set new url for AWS server

--- prototype/flutter/app/lib/services/socket-client.service.dart
+++ prototype/flutter/app/lib/services/socket-client.service.dart
@@ -2,7 +2,8 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketClient {
   late IO.Socket socket;
-  String serverUrl = 'http://10.0.0.9:3000';
+  String serverUrl =
+      'http://ec2-3-99-132-207.ca-central-1.compute.amazonaws.com:3000';
 
   SocketClient() {
     socket = IO.io(serverUrl, <String, dynamic>{
