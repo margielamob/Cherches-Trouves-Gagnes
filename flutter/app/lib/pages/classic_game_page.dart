01d9e7d52e8b7305698a7894ab302d9275937e63
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-10-12 23:11:15 -0400

    use provider instead of using statefull widget because it was rephreshing the whole widget

--- flutter/app/lib/pages/classic_game_page.dart
+++ flutter/app/lib/pages/classic_game_page.dart
@@ -2,33 +2,54 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:app/services/image_loader_service.dart';
 import 'package:app/components/game_vignette/game_vignette.dart';
+import 'package:provider/provider.dart';
+
+class Coordinate extends ChangeNotifier {
+  double x = 0.0;
+  double y = 0.0;
+
+  void setCoordinate(TapUpDetails details) {
+    x = details.localPosition.dx;
+    y = details.localPosition.dy;
+    notifyListeners();
+  }
+}
 
 class Classic extends StatelessWidget {
   final ImageLoaderService _imageLoaderService = ImageLoaderService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Classic'),
-      ),
-      body: Center(
-        child: FutureBuilder<ui.Image>(
-          future: _imageLoaderService.loadImage('difference.bmp'),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              final image = snapshot.data;
-              if (image != null) {
-                return Column(
-                  children: <Widget>[
-                    GestureDetector(child: GameVignette(image)),
-                    Text("we are fuckie")
-                  ],
-                );
+    return ChangeNotifierProvider(
+      create: (_) => Coordinate(),
+      builder: (context, child) => Scaffold(
+        appBar: AppBar(
+          title: Text('Classic'),
+        ),
+        body: Center(
+          child: FutureBuilder<ui.Image>(
+            future: _imageLoaderService.loadImage('difference.bmp'),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                final image = snapshot.data;
+                if (image != null) {
+                  return Column(
+                    children: <Widget>[
+                      GestureDetector(
+                          onTapUp: (details) =>
+                              Provider.of<Coordinate>(context, listen: false)
+                                  .setCoordinate(details),
+                          child: GameVignette(image)),
+                      Consumer<Coordinate>(
+                          builder: (_, coordinate, __) => Text(
+                              "X coordinate : ${coordinate.x} and ${coordinate.y}"))
+                    ],
+                  );
+                }
               }
-            }
-            return CircularProgressIndicator();
-          },
+              return CircularProgressIndicator();
+            },
+          ),
         ),
       ),
     );
