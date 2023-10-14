b3706552e4a85a5f067e76c2bf73d10a26c77f28
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-10-14 09:31:05 -0400

    centering the cards

--- flutter/app/lib/components/carrousel/carrousel_card.dart
+++ flutter/app/lib/components/carrousel/carrousel_card.dart
@@ -7,39 +7,42 @@ class CarrouselCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Card(
-        margin: EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              data.title,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1.0,
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Card(
+            margin: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  data.title,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              child: SizedBox(
-                width: 320,
-                height: 240,
-                child: Image.network(
-                  data.thumbnail,
-                  fit: BoxFit.cover,
+                SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: SizedBox(
+                    width: 320,
+                    height: 240,
+                    child: Image.network(
+                      data.thumbnail,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
-    ]);
+          ),
+        ]);
   }
 }
