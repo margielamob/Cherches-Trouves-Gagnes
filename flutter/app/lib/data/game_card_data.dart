ccdd130eec8b7948a3a670fbad0f3f602e077b3c
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-10-14 18:48:14 -0400

    create gamecard data

--- flutter/app/lib/data/game_card_data.dart
+++ flutter/app/lib/data/game_card_data.dart
@@ -5,11 +5,17 @@ class GameCardData {
   String id;
   String title;
   Uint8List thumbnail;
+  int nbDifferences;
+  String idEditedBmp;
+  String idOriginalBmp;
 
   GameCardData({
     required this.id,
     required this.title,
     required this.thumbnail,
+    required this.nbDifferences,
+    required this.idEditedBmp,
+    required this.idOriginalBmp,
   });
 
   factory GameCardData.fromJson(Map json) {
@@ -17,6 +23,9 @@ class GameCardData {
       id: json['id'],
       title: json['name'],
       thumbnail: base64Decode(json['thumbnail'].split(',').last),
+      nbDifferences: json['nbDifferences'],
+      idEditedBmp: json['idEditedBmp'],
+      idOriginalBmp: json['idOriginalBmp'],
     );
   }
 }
