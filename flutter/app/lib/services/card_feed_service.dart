425a571129be7c75f835b5020a1cc095ea586b69
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-10-14 11:53:05 -0400

    remove junk from card service

--- flutter/app/lib/services/card_feed_service.dart
+++ flutter/app/lib/services/card_feed_service.dart
@@ -1,11 +1,9 @@
-import 'package:app/data/carrousel_request_data.dart';
 import 'package:app/data/game_card_data.dart';
 import 'package:app/services/http_client_service.dart';
 import 'package:get_it/get_it.dart';
 
 class CardFeedService {
   int _currentPage = 0;
-  List<GameCardData> _currentCards = [];
   final HttpClientService _httpClientService = GetIt.I.get<HttpClientService>();
 
   Future<List<GameCardData>> getCurrentPageCards() async {
@@ -17,10 +15,6 @@ class CardFeedService {
       print(error);
       throw Exception('could not load and parse cards');
     }
-  }
-
-  List<GameCardData> getCurrentCards() {
-    return _currentCards;
   }
 
   int getCurrentPage() {
