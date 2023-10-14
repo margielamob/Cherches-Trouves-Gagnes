b04a35398c45df73fafde38b091535497701efa9
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-10-14 10:39:27 -0400

    remove comments

--- flutter/app/lib/components/carrousel/carrousel_card.dart
+++ flutter/app/lib/components/carrousel/carrousel_card.dart
@@ -13,65 +13,60 @@ class CarrouselCard extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Card(
-                // elevation: 2.0,
-                shadowColor: Color.fromARGB(255, 46, 46, 46),
-                margin: EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        data.title,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 1.0,
-                          ),
-                        ),
-                        child: SizedBox(
-                          width: 320,
-                          height: 240,
-                          child: Image.network(
-                            data.thumbnail,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 0.0),
-                        child: Row(
-                          children: [
-                            FilledButton(
-                              onPressed: onPressed,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Text("Supprimer "),
-                                  Icon(Icons.delete),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Card(
+            shadowColor: Color.fromARGB(255, 46, 46, 46),
+            margin: EdgeInsets.all(8.0),
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    data.title,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
+                  SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1.0,
+                      ),
+                    ),
+                    child: SizedBox(
+                      width: 320,
+                      height: 240,
+                      child: Image.network(
+                        data.thumbnail,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 0.0),
+                    child: Row(
+                      children: [
+                        FilledButton(
+                          onPressed: onPressed,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text("Supprimer "),
+                              Icon(Icons.delete),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
-            ])
+            ),
+          ),
+        ])
       ],
     );
   }
