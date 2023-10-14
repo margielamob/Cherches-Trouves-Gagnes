c56ad0d7a79a150b614b3247a75e4c9c2926a0a0
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-10-14 09:36:20 -0400

    add padding to the cards

--- flutter/app/lib/components/carrousel/carrousel_card.dart
+++ flutter/app/lib/components/carrousel/carrousel_card.dart
@@ -13,33 +13,36 @@ class CarrouselCard extends StatelessWidget {
         children: [
           Card(
             margin: EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Text(
-                  data.title,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 1.0,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    data.title,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  child: SizedBox(
-                    width: 320,
-                    height: 240,
-                    child: Image.network(
-                      data.thumbnail,
-                      fit: BoxFit.cover,
+                  SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1.0,
+                      ),
+                    ),
+                    child: SizedBox(
+                      width: 320,
+                      height: 240,
+                      child: Image.network(
+                        data.thumbnail,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ]);
