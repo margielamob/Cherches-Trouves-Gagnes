2439f9f2f7d1c93e599c89d408f896e25058f75a
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-10-14 20:56:06 -0400

    commenting second service, we'll need to use provider, getIt seems to not work correctly

--- flutter/app/lib/services/classic_game_service.dart
+++ flutter/app/lib/services/classic_game_service.dart
@@ -1,11 +1,10 @@
-import 'dart:convert';
 import 'package:app/services/http_service.dart';
-import 'package:http/http.dart' as http;
+import 'package:app/services/image_decoder_service.dart';
 import 'package:get_it/get_it.dart';
 
 class ClassicGameService {
   final httpService = GetIt.I.get<HttpService>();
-  
+  //final imageDecoderService = GetIt.I.get<ImageDecoderService>();
 
   // Responsable d'avoir deux images avec les différences.
 }
