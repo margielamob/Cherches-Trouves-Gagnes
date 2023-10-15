cd8e176f138182db68a36d3e14a01a9f8559ccdf
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-10-14 23:24:57 -0400

    remove second future builder

--- flutter/app/lib/pages/classic_game_page.dart
+++ flutter/app/lib/pages/classic_game_page.dart
@@ -47,37 +47,6 @@ class Classic extends StatelessWidget {
               return CircularProgressIndicator();
             },
           ),
-          SizedBox(width: 20),
-          FutureBuilder<ui.Image>(
-            future: _classicGameService
-                .getImageFromId("f8442f87-27c7-4353-8c20-b7e83708af9c"),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                final image = snapshot.data;
-                if (image != null) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        children: <Widget>[
-                          GestureDetector(
-                            onTapUp: (details) {
-                              x1.value = details.localPosition.dx;
-                              y1.value = details.localPosition.dy;
-                            },
-                            child: GameVignette(image),
-                          ),
-                          Obx(() => Text(
-                              "Coordinate x : ${x1.value}, y : ${y1.value}"))
-                        ],
-                      ),
-                    ],
-                  );
-                }
-              }
-              return CircularProgressIndicator();
-            },
-          ),
         ]),
       ),
     );
