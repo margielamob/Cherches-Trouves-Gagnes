8f325d2f57054eb125a4231fe3f1774e2c9cb8f2
Thierry Beaulieu <thierry.abeaulieu@gmail.com>
2023-10-21 05:11:10 +0000

    Example of unit tests

--- flutter/app/lib/domain/services/example_service.dart
+++ flutter/app/lib/domain/services/example_service.dart
@@ -1,6 +1,4 @@
-import 'package:app/domain/services/http_service.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class ExampleState {
   int counter = 0;
@@ -8,7 +6,6 @@ class ExampleState {
 
 class ExampleService extends ChangeNotifier {
   ExampleState state = ExampleState();
-  HttpService httpService = Get.find();
 
   incrementCounter() {
     state.counter++;
