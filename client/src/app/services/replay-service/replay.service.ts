b63b0b6380700fb4f4ddcb603c4c904c753d2155
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-10-28 15:13:43 -0400

    add parameter of ReplayService

--- client/src/app/services/replay-service/replay.service.ts
+++ client/src/app/services/replay-service/replay.service.ts
@@ -1,12 +1,25 @@
-/* eslint-disable max-params */
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { ReplayEvent } from '@app/interfaces/replay-actions';
+import { ReplayInterval } from '@app/interfaces/replay-interval';
+import { Coordinate } from '@common/coordinate';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
-export class ReplayService {
+export class ReplayService implements OnDestroy {
     isReplaying: boolean = false;
+    private replayEvents: ReplayEvent[];
+    private replaySpeed: number;
+    private currentCoords: Coordinate[];
+    private isCheatMode: boolean;
+    private isDifferenceFound: boolean;
+    private replayInterval: ReplayInterval;
+    private currentReplayIndex: number;
+    private replayTimer: BehaviorSubject<number>;
+    private replayDifferenceFound: BehaviorSubject<number>;
+    private replayOpponentDifferenceFound: BehaviorSubject<number>;
+    private replayEventsSubjectSubscription: Subscription;
 
-    // eslint-disable-next-line @typescript-eslint/no-useless-constructor, @typescript-eslint/no-empty-function
     constructor() {}
 }
