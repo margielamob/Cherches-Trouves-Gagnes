19c00cfe08d1ff7ecceea6185b7c98d3097a1ca1
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-11-07 13:31:31 -0500

    apply linter on client side

--- client/src/app/services/joinable-game/joinable-game.service.spec.ts
+++ client/src/app/services/joinable-game/joinable-game.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { JoinableGameService } from './joinable-game.service';
 
 describe('JoinableGameService', () => {
-  let service: JoinableGameService;
+    let service: JoinableGameService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(JoinableGameService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(JoinableGameService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
