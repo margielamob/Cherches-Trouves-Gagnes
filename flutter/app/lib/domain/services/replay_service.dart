e99bf14863199af1bfd0521ab724ebde53834001
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2023-11-07 13:00:06 -0500

    add a replayState

--- flutter/app/lib/domain/services/replay_service.dart
+++ flutter/app/lib/domain/services/replay_service.dart
@@ -1,5 +1,7 @@
 import 'package:app/domain/models/event_queue.dart';
+import 'package:app/domain/models/replay_state.dart';
 
 class ReplayService {
   final eventQueue = EventQueue();
+  final replayState = ReplayState.stopped;
 }
--- flutter/app/test/service/replay_service_test.dart
+++ flutter/app/test/service/replay_service_test.dart
@@ -1,4 +1,5 @@
 import 'package:app/domain/models/event_queue.dart';
+import 'package:app/domain/models/replay_state.dart';
 import 'package:app/domain/services/replay_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
@@ -17,6 +18,12 @@ void main() {
     test('replayService should have an eventQueue', () {
       final replayService = Get.find<ReplayService>();
       expect(replayService.eventQueue, isA<EventQueue>());
+    });
+
+    test('replayState of replayService should by default be on pause', () {
+      final replayService = Get.find<ReplayService>();
+      expect(replayService.replayState, isA<ReplayState>());
+      expect(replayService.replayState, ReplayState.stopped);
     });
   });
 }
