6e55b6b9fe0809ae0aef2786e40904208c77955e
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-11-11 14:32:07 -0500

    DialogBox to set params of the game done on client léger

--- flutter/app/lib/components/game_constants_dialog.dart
+++ flutter/app/lib/components/game_constants_dialog.dart
@@ -2,6 +2,7 @@ import 'package:app/domain/models/game_card_model.dart';
 import 'package:app/domain/services/auth_service.dart';
 import 'package:app/domain/services/game_manager_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
@@ -16,59 +17,82 @@ class GameConstantsDialog extends StatefulWidget {
 }
 
 class _GameConstantsDialogState extends State<GameConstantsDialog> {
-  int selectedNumber = 200;
-  bool checkboxValue = true;
+  int gameTimer = 200;
+  bool cheatModeActivated = true;
   @override
   Widget build(BuildContext context) {
     final gameManager = Provider.of<GameManagerService>(context);
     return AlertDialog(
       title: Text("Configure les paramètres de la partie"),
-      content: Column(
-        children: [
-          // Number input
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Sélectionnez un nombre :"),
-              SizedBox(
-                width: 100,
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  onChanged: (value) {
-                    selectedNumber = int.tryParse(value) ?? selectedNumber;
-                  },
-                  decoration: InputDecoration(
-                    hintText: "10",
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-        ],
-      ),
       actions: <Widget>[
-        Row(
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FilledButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(Size(100.0, 40.0)),
+            Text("Durée de la partie (secondes) :"),
+            SizedBox(
+              width: 50,
+              child: TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  gameTimer = int.tryParse(value) ?? gameTimer;
+                  setState(() {
+                    gameTimer = gameTimer < 10 ? 10 : gameTimer;
+                  });
+                },
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'^\d+')),
+                  FilteringTextInputFormatter.deny(RegExp(r'^[0]{2,}')),
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(4),
+                ],
+                decoration: InputDecoration(
+                  hintText: "200",
+                ),
               ),
-              child: Text("Annuler"),
             ),
-            SizedBox(width: 20),
-            FilledButton(
-              onPressed: () {
-                gameManager.createMultiplayerGame(widget.data.id, true, 100);
-                gameManager.currentGameId = widget.data.id;
-                gameManager.gameCards = widget.data;
-              },
-              style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(Size(100.0, 40.0)),
-              ),
-              child: Text("Confirmer"),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Activer le cheat mode :"),
+                Checkbox(
+                  value: cheatModeActivated,
+                  onChanged: (value) {
+                    setState(() {
+                      cheatModeActivated = value ?? cheatModeActivated;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilledButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(Size(100.0, 40.0)),
+                  ),
+                  child: Text("Annuler"),
+                ),
+                SizedBox(width: 20),
+                FilledButton(
+                  onPressed: () {
+                    print(gameTimer);
+                    gameManager.createMultiplayerGame(
+                        widget.data.id, cheatModeActivated, gameTimer);
+                    gameManager.currentGameId = widget.data.id;
+                    gameManager.gameCards = widget.data;
+                  },
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(Size(100.0, 40.0)),
+                  ),
+                  child: Text("Confirmer"),
+                ),
+              ],
             ),
           ],
         ),
