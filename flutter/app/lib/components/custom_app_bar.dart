b8f4928799d0e8fbe4f13e3956cc67784db9e39f
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-11-12 14:00:23 -0500

    fix own bug

--- flutter/app/lib/components/custom_app_bar.dart
+++ flutter/app/lib/components/custom_app_bar.dart
@@ -1,23 +1,17 @@
 import 'package:app/components/logout_dialog.dart';
 import 'package:app/domain/services/difference_detection_service.dart';
-import 'package:app/domain/services/game_manager_service.dart';
 import 'package:app/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CustomAppBar {
   static AppBar buildDefaultBar(context, String pageName) {
-    final GameManagerService gameManagerService = Get.find();
-    bool isGamePage = Get.currentRoute == '/Classic';
     return AppBar(
       title: Text(pageName),
       actions: [
         IconButton(
           icon: Icon(Icons.home),
           onPressed: () {
-            if (isGamePage) {
-              gameManagerService.leavingArena();
-            }
             Get.offAll(MainPage(), transition: Transition.leftToRight);
           },
         ),
