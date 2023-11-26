20cee75d5e43ace59bdd38eb6abfe961622084e4
Skander Hannachi <hannachiskander@live.ca>
2023-11-26 06:04:34 +0000

    Feature/rectangle draw

--- client/src/app/interfaces/command.ts
+++ client/src/app/interfaces/command.ts
@@ -7,4 +7,14 @@ export interface Command {
     name: string;
     strokes: Stroke[];
     style: StrokeStyle;
+    rectangleStart?: { x: number; y: number };
+    rectangleWidth?: number;
+    rectangleHeight?: number;
+    ellipseStart?: { x: number; y: number };
+    centerX?: number;
+    centerY?: number;
+    radiusX?: number;
+    radiusY?: number;
+    ellipseHeight?: number;
+    ellipseWidth?: number;
 }
