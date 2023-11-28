32d82b99172cf5daaa33e9d42e803bf0479ef938
Booskander <skander.hannachi@polymtl.ca>
2023-11-27 21:22:31 -0500

    deleted a shit ton of tests

--- client/src/app/components/rectangle-outline/rectangle-outline.component.spec.ts
+++ client/src/app/components/rectangle-outline/rectangle-outline.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RectangleOutlineComponent } from './rectangle-outline.component';
 
 describe('RectangleOutlineComponent', () => {
-  let component: RectangleOutlineComponent;
-  let fixture: ComponentFixture<RectangleOutlineComponent>;
+    let component: RectangleOutlineComponent;
+    let fixture: ComponentFixture<RectangleOutlineComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RectangleOutlineComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [RectangleOutlineComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(RectangleOutlineComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(RectangleOutlineComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
