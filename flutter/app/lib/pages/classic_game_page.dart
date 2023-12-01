69c58233d7fa2b532f29f0a1798f9fe4a27fe4db
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-12-01 14:09:45 -0500

    fix env prod

--- flutter/app/lib/pages/classic_game_page.dart
+++ flutter/app/lib/pages/classic_game_page.dart
@@ -50,7 +50,6 @@ class _ClassicState extends State<Classic> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     chatDisplaySubscription?.cancel();
     unreadMessagesSubscription?.cancel();
