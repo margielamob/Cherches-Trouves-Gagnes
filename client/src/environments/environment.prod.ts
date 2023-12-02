dc8b456f34e1a15249db239290768718a3163c8b
Ahmed El Shami <ahmed.el-shami@polymtl.ca>
2023-12-02 00:12:45 +0000

    good morning america

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -8,6 +8,6 @@ export const environment = {
         messagingSenderId: '277200615128',
     },
     production: true,
-    serverUrl: 'http://ec2-3-99-132-207.ca-central-1.compute.amazonaws.com:3000/api',
-    socketUrl: 'http://ec2-3-99-132-207.ca-central-1.compute.amazonaws.com:3000',
+    serverUrl: 'http://localhost:3000/api',
+    socketUrl: 'http://localhost:3000',
 };
